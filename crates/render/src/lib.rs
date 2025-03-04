5080d158cf0ad03ebfa155e01e8b66ccb0943648
r. keith <keithroe@gmail.com>
2025-03-04 13:04:41 -0700

    Back to solid render of basic model with new model/scene structure

--- crates/render/src/lib.rs
+++ crates/render/src/lib.rs
@@ -2,37 +2,36 @@
 
 mod camera;
 
-use wgpu::util::DeviceExt;
+use wgpu::{util::DeviceExt, PrimitiveTopology};
 
 mod light {
     pub struct UniformData {
-        enabled: bool,
-        dir: glam::Vec3,
-        color: glam::Vec3,
-        scale: f32,
-        ambient: glam::Vec3,
-        ambient_scale: f32,
+        _enabled: bool,
+        _dir: glam::Vec3,
+        _color: glam::Vec3,
+        _scale: f32,
+        _ambient: glam::Vec3,
+        _ambient_scale: f32,
     }
 
     impl Default for UniformData {
         fn default() -> Self {
             Self {
-                enabled: true,
-                dir: glam::Vec3::new(-1.0, -1.0, -1.0).normalize(),
-                color: glam::Vec3::new(0.9, 0.6, 0.3),
-                scale: 0.7,
-                ambient: glam::Vec3::new(0.5, 0.5, 0.7),
-                ambient_scale: 0.3,
+                _enabled: true,
+                _dir: glam::Vec3::new(-1.0, -1.0, -1.0).normalize(),
+                _color: glam::Vec3::new(0.9, 0.6, 0.3),
+                _scale: 0.7,
+                _ambient: glam::Vec3::new(0.5, 0.5, 0.7),
+                _ambient_scale: 0.3,
             }
         }
     }
 
     #[derive(Default)]
     pub struct Light {
-        uniform_data: UniformData,
+        _uniform_data: UniformData,
     }
 }
-
 /// Simple renderer for single 3D model
 ///
 /// Handles user events (eg, mouse drag) and renders model to offscreen texture.
@@ -43,6 +42,8 @@ pub struct Renderer {
     render_view: RenderView,
     shader_module: wgpu::ShaderModule,
     scene: Scene,
+
+    render_pipelines: Vec<wgpu::RenderPipeline>,
 }
 
 impl Renderer {
@@ -61,12 +62,15 @@ impl Renderer {
 
         let scene = Scene::from_model(input_model, &device);
 
+        let render_pipelines = generate_pipelines(&scene, &shader_module, &render_view, &device);
+
         Self {
             queue,
             device,
             render_view,
             shader_module,
             scene,
+            render_pipelines,
         }
     }
 
@@ -75,7 +79,49 @@ impl Renderer {
     }
 
     pub fn render(&self) {
-        todo!()
+        for (mesh_idx, mesh) in self.scene.meshes.iter().enumerate() {
+            let mut encoder = self
+                .device
+                .create_command_encoder(&wgpu::CommandEncoderDescriptor {
+                    label: Some("Render Encoder"),
+                });
+            {
+                let camera_matrix = self.scene.camera.controller.view_projection_matrix();
+                self.queue.write_buffer(
+                    &self.scene.camera.buffer,
+                    0,
+                    bytemuck::cast_slice(&[camera_matrix]),
+                );
+                let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+                    label: Some("Render Pass"),
+                    color_attachments: &[
+                        // This is what @location(0) in the fragment shader targets
+                        Some(wgpu::RenderPassColorAttachment {
+                            view: &self.render_view.view,
+                            resolve_target: None,
+                            ops: wgpu::Operations {
+                                load: wgpu::LoadOp::Clear(wgpu::Color {
+                                    r: 0.1,
+                                    g: 0.2,
+                                    b: 0.3,
+                                    a: 1.0,
+                                }),
+                                store: wgpu::StoreOp::Store,
+                            },
+                        }),
+                    ],
+                    depth_stencil_attachment: None,
+                    timestamp_writes: None,
+                    occlusion_query_set: None,
+                });
+                render_pass.set_bind_group(0, &self.scene.camera.bind_group, &[]);
+                render_pass.set_pipeline(&self.render_pipelines[mesh_idx]); // 2.
+                render_pass.set_vertex_buffer(0, mesh.vertex_buffer.slice(..));
+                //render_pass.draw(0..3, 0..1);
+                render_pass.draw(0..mesh.num_triangles, 0..1);
+            }
+            self.queue.submit(std::iter::once(encoder.finish()));
+        }
     }
 
     pub fn handle_event(&mut self, event: event::Event) {
@@ -133,8 +179,8 @@ impl Renderer {
 struct Scene {
     bbox: model::BoundingBox,
     camera: WGPUCamera,
-    light: WGPULight,
-    materials: Vec<WGPUMaterial>,
+    _light: WGPULight,
+    _materials: Vec<WGPUMaterial>,
     skins: Vec<WGPUSkin>,
     meshes: Vec<WGPUMesh>,
 }
@@ -144,8 +190,8 @@ impl Scene {
         let mut scene = Self {
             bbox: model.bbox,
             camera: WGPUCamera::with_looking_at(&model.bbox, device),
-            light: WGPULight::new(device),
-            materials: Vec::new(),
+            _light: WGPULight::new(device),
+            _materials: Vec::new(),
             skins: Vec::new(),
             meshes: Vec::new(),
         };
@@ -169,9 +215,7 @@ impl Scene {
     }
 }
 
-///
-///
-///
+/// Camera controller and WGPU state for device-side camera data
 struct WGPUCamera {
     controller: camera::Camera,
     buffer: wgpu::Buffer,
@@ -235,13 +279,11 @@ impl WGPUCamera {
     }
 }
 
-///
-///
-///
+/// Light controller and its WGPU state for device-side light data
 struct WGPULight {
-    controller: light::Light,
-    buffer: wgpu::Buffer,
-    bind_group: wgpu::BindGroup,
+    _controller: light::Light,
+    _buffer: wgpu::Buffer,
+    _bind_group: wgpu::BindGroup,
 }
 
 impl WGPULight {
@@ -266,9 +308,9 @@ impl WGPULight {
         });
 
         Self {
-            controller,
-            buffer,
-            bind_group,
+            _controller: controller,
+            _buffer: buffer,
+            _bind_group: bind_group,
         }
     }
 }
@@ -358,6 +400,7 @@ impl RenderView {
     }
 
     /// return render texture dimensions (width, height)
+    #[allow(unused)]
     fn size(&self) -> glam::UVec2 {
         glam::UVec2::new(self.desc.size.width, self.desc.size.height)
     }
@@ -368,6 +411,102 @@ impl RenderView {
     }
 }
 
+fn generate_pipelines(
+    scene: &Scene,
+    shader_module: &wgpu::ShaderModule,
+    render_view: &RenderView,
+    device: &wgpu::Device,
+) -> Vec<wgpu::RenderPipeline> {
+    let mut pipelines = Vec::new();
+
+    for mesh in &scene.meshes {
+        // build render pipeline layout
+        let bind_group_layouts = [&WGPUCamera::bind_group_layout(device)];
+        let render_pipeline_layout =
+            device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+                label: Some("Render Pipeline Layout"),
+                bind_group_layouts: &bind_group_layouts,
+                push_constant_ranges: &[],
+            });
+
+        // build pipeline
+        let mut vertex_buffer_layouts = vec![wgpu::VertexBufferLayout {
+            array_stride: std::mem::size_of::<glam::Vec3>() as wgpu::BufferAddress,
+            step_mode: wgpu::VertexStepMode::Vertex,
+            attributes: &[wgpu::VertexAttribute {
+                offset: 0,
+                shader_location: 0,
+                format: wgpu::VertexFormat::Float32x3,
+            }],
+        }];
+
+        //
+        // TODO: add other vertex attribute buffers
+        //
+
+        if let Some(index_buffer) = &mesh.index_buffer {
+            todo!();
+        }
+
+        let vertex_state = wgpu::VertexState {
+            module: shader_module,
+            entry_point: Some("vs_main"),
+            buffers: &vertex_buffer_layouts,
+            compilation_options: Default::default(),
+        };
+
+        let fragment_state = wgpu::FragmentState {
+            module: shader_module,
+            entry_point: Some("fs_main"),
+            targets: &[Some(wgpu::ColorTargetState {
+                format: render_view.desc.format,
+                blend: Some(wgpu::BlendState {
+                    color: wgpu::BlendComponent::REPLACE,
+                    alpha: wgpu::BlendComponent::REPLACE,
+                }),
+                write_mask: wgpu::ColorWrites::ALL,
+            })],
+            compilation_options: Default::default(),
+        };
+
+        let primitive_state = wgpu::PrimitiveState {
+            topology: wgpu::PrimitiveTopology::TriangleList,
+            strip_index_format: None,
+            front_face: wgpu::FrontFace::Ccw,
+            cull_mode: None,
+            polygon_mode: wgpu::PolygonMode::Fill,
+            // Requires Features::DEPTH_CLIP_CONTROL
+            unclipped_depth: false,
+            // Requires Features::CONSERVATIVE_RASTERIZATION
+            conservative: false,
+        };
+
+        let multisample_state = wgpu::MultisampleState {
+            count: 1,
+            mask: !0,
+            alpha_to_coverage_enabled: false,
+        };
+
+        let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+            label: Some("Render Pipeline"),
+            layout: Some(&render_pipeline_layout),
+            vertex: vertex_state,
+            fragment: Some(fragment_state),
+            primitive: primitive_state,
+            depth_stencil: None,
+            multisample: multisample_state,
+            // If the pipeline will be used with a multiview render pass, this
+            // indicates how many array layers the attachments will have.
+            multiview: None,
+            // Useful for optimizing shader compilation on Android
+            cache: None,
+        });
+
+        pipelines.push(render_pipeline);
+    }
+
+    pipelines
+}
 /*
 pub struct Renderer {
     queue: wgpu::Queue,
