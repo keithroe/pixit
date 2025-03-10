a8632a4852d423d9cccf497d2c06196be406525e
keith <keithroe@gmail.com>
2025-03-10 14:24:47 -0600

    Forgot tadd texture.rs in last commit

--- crates/app/src/main.rs
+++ crates/app/src/main.rs
@@ -1,9 +1,20 @@
 #![warn(clippy::all, rust_2018_idioms)]
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 
+use clap::Parser;
 mod app;
 
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Path to gltf/glb file to be rendered
+    #[arg(short, long)]
+    gltf: String,
+}
+
 fn main() -> eframe::Result {
+    //let args = Args::parse();
+
     env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
 
     let native_options = eframe::NativeOptions {
